02025018f4943bb2cc1c3264bc056a90a2f10d80
Alex Delgado <alex.delgado@fowardslash.com>
2020-03-25 11:22:51 -0500

    Setting up React Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+
+import store from './js/store';
+import App from './js/components/app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/js/constants/index.js
@@ -0,0 +1,1 @@
+// src/js/constants/index.js
