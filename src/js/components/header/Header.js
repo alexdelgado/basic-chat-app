8772e748c4ef79085d62ed69ab4337558055f5ef
Alex Delgado <alex.delgado@fowardslash.com>
2020-03-25 12:00:37 -0500

    Adding content to header

--- src/js/components/header/Header.js
+++ src/js/components/header/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function Header() {
+import AddChatBtn from './AddChatBtn'
+
+function Header(props) {
   return (
     <header className="chat__header">
+      <h1>{props.title}</h1>
+      <AddChatBtn label="Add chat" />
     </header>
   );
 }
