fa2f40723efad99b71d269c5666b9d9ce80f1600
Alex Delgado <alex.delgado@fowardslash.com>
2020-03-25 14:10:18 -0500

    Styling header

--- src/js/components/header/Header.js
+++ src/js/components/header/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import AddChatBtn from './AddChatBtn'
 
+import './Header.css'
+
 function Header(props) {
   return (
-    <header className="chat__header">
+    <header className="chat__header header">
       <h1>{props.title}</h1>
       <AddChatBtn label="Add chat" />
     </header>
